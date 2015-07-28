89eae38d468d50d28066ce4e7871f6c79f868402
ayelet-seeman <ayeletseeman@gmail.com>
2015-07-28 19:52:06 +0300

    Create importace.1.0.js

---
+++ importace.1.0.js
@@ -0,0 +1,32 @@
+ var cogaProfile = {
+	 "test":"success",
+
+"ariaImportance":
+{
+	"one":
+	{
+		"settings":{
+			"ariaHidden": "false",
+		}
+	},
+
+	"two":
+	{
+		"settings":{
+			"ariaHidden": "true",		}
+	},
+	"three":
+	{
+		"settings":{
+			"ariaHidden": "true"
+		},
+	},
+	"four":
+	{
+		"settings":{
+			"ariaHidden": "true"
+		},
+	},
+	
+	}
+ }
