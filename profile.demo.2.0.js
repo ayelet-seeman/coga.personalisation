fc107f091b94b459f29f02f330c36680cd6a71b8
ayelet-seeman <ayeletseeman@gmail.com>
2015-07-28 23:59:49 +0300

    Create profile.demo.2.0.js

---
+++ profile.demo.2.0.js
@@ -0,0 +1,32 @@
+ var cogaProfile = {
+	 "test":"success",
+
+"ariaImportance":
+{
+	"one":
+	{
+		"settings":{
+			"ariaHidden": "false",
+		}
+	},
+
+	"two":
+	{
+		"settings":{
+			"ariaHidden": "false",		}
+	},
+	"three":
+	{
+		"settings":{
+			"ariaHidden": "true"
+		},
+	},
+	"four":
+	{
+		"settings":{
+			"ariaHidden": "true"
+		},
+	},
+	
+	}
+ }
